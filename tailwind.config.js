09f772888ea6c2c776e10e10c707a0424714527c
Your Name <your-email@example.com>
2025-11-19 16:58:50 -0600

    Configure Node version for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
-module.exports = {
-  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
